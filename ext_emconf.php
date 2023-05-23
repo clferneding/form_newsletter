498d690021a1daf9491b66c293a645f72f7467a4
Claudia Ferneding <claudia.ferneding@leg-thueringen.de>
2023-05-23 16:16:46 +0200

    TYPO3 11.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -16,8 +16,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.4.1',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.0-10.4.99',
-            'form' => '8.7.0-10.4.99'
+            'typo3' => '8.7.0-11.5.99',
+            'form' => '8.7.0-11.5.99'
         ],
         'conflicts' => [],
         'suggests' => [],
